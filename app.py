e5dcd654698919c50e9efbdd92e4ce3d9d24150d
Bidhatri Amatya <abidhatri@gmail.com>
2024-11-10 08:33:56 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# from flask import Flask, render_template
+# import pandas as pd
+
+# app = Flask(__name__)
+
+# # Load the saved results
+# data = pd.read_csv('predictions_data.csv')
+
+# @app.route('/')
+# def index():
+#     # You can display the top rows or any specific data you want
+#     return render_template('index.html', data=data.head())
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
